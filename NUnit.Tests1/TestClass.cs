2bd0080a11c1cbfaf21dfda7b4d2c7f2c34e0780
Andriy Zymenko <andriy.zymenko@gmail.com>
2017-02-14 15:49:00 +0200

    feature done

---
+++ NUnit.Tests1/TestClass.cs
@@ -0,0 +1,22 @@
+﻿using NUnit.Framework;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using OpenOfficeWpfApp;
+
+namespace NUnit.Tests1
+{
+    [TestFixture]
+    public class TestClass
+    {
+        [Test]
+        public void TestChangePackageMethod()
+        {
+            var testOfficePackage = new ChangeTestOfficeFileClass();
+            testOfficePackage.ChangePackage(@"C:\Backup\test4.docx");
+            //Assert.Pass("ChangePackage passing test");
+        }
+    }
+}
